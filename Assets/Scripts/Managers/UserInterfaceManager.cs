8e517e51504e339bed75a2f4e0ddac2755d68be5
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-02-19 21:58:10 +0300

    added user interface manager

---
+++ Assets/Scripts/Managers/UserInterfaceManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/*
+ И интерфейс позволяющий игроку цветами манипулировать — так же удалять и добавлять.
+2. В проекте должен быть интерфейс для манипуляции с «инвентарём».
+ */
+
+namespace Managers
+{
+    public class UserInterfaceManager : MonoBehaviour
+    {
+
+    }
+}
