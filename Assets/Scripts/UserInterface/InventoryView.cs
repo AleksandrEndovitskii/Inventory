4182b2bb6e9660ee44063257479aec9cb8bcc32e
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-02-19 22:43:34 +0300

    added inventory

---
+++ Assets/Scripts/UserInterface/InventoryView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace UserInterface
+{
+    public class InventoryView : MonoBehaviour
+    {
+        public Color SelectedColor;
+
+        public void OnAddColorButtonClick()
+        {
+
+        }
+
+        public void OnRemoveColorButtonClick()
+        {
+
+        }
+
+        public void OnSaveColorButtonClick()
+        {
+
+        }
+    }
+}
