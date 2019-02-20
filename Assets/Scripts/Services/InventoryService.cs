73cd799bd15f8da70eb7f94a08a873561c41a123
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-02-20 03:34:02 +0300

    re-implemented actions with inventory service

--- Assets/Scripts/Services/InventoryService.cs
+++ Assets/Scripts/Services/InventoryService.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using InventoryItems;
 using UnityEngine;
 using Utilities;
@@ -22,13 +20,13 @@ namespace Services
         {
             InventoryItems = new ObservableCollection<IInventoryItem>();
 
-            Add(new InventoryColor(Color.red));
-            Add(new InventoryColor(Color.green));
-            Add(new InventoryColor(Color.blue));
-            Add(new InventoryColor(Color.black));
-            Add(new InventoryColor(Color.black));
-            Add(new InventoryColor(Color.white));
-            Add(new InventoryColor(Color.white));
+            TryAdd(new InventoryColor(Color.red));
+            TryAdd(new InventoryColor(Color.green));
+            TryAdd(new InventoryColor(Color.blue));
+            TryAdd(new InventoryColor(Color.black));
+            TryAdd(new InventoryColor(Color.black));
+            TryAdd(new InventoryColor(Color.white));
+            TryAdd(new InventoryColor(Color.white));
         }
 
         public void Uninitialize()
@@ -38,8 +36,29 @@ namespace Services
             InventoryItems = null;
         }
 
-        public bool Add(InventoryColor inventoryColor)
+        public bool TryAdd(IInventoryItem inventoryItem)
         {
+            if (inventoryItem is InventoryColor inventoryColor)
+            {
+                return TryAdd(inventoryColor);
+            }
+
+            return false; // TODO
+        }
+
+        public bool TryAdd(InventoryColor inventoryColor)
+        {
+            Debug.Log("Trying to add " + inventoryColor.Color + "to inventory");
+            var result = "";
+            foreach (var inventoryItem in InventoryItems)
+            {
+                if (inventoryItem is InventoryColor ic)
+                {
+                    result += ic.Color + " ";
+                }
+            }
+            Debug.Log("Content of inventory: " + result);
+
             if (InventoryItems.Contains(inventoryColor))
             {
                 Debug.Log("Inventory already contain this color");
@@ -51,7 +70,17 @@ namespace Services
             return true;
         }
 
-        public bool Remove(InventoryColor inventoryColor)
+        public bool TryRemove(IInventoryItem inventoryItem)
+        {
+            if (inventoryItem is InventoryColor inventoryColor)
+            {
+                return TryRemove(inventoryColor);
+            }
+
+            return false; // TODO
+        }
+
+        public bool TryRemove(InventoryColor inventoryColor)
         {
             var result = InventoryItems.Remove(inventoryColor);
 
