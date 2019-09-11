70d77b82e10c3b1fe054b51149e09b256f7c8942
Aleksandr <endovitsky.a.p@gmail.com>
2019-09-11 18:36:24 +0300

    added selected item image component

---
+++ Assets/Scripts/Components/SelectedInventoryItemImageComponent.cs
@@ -0,0 +1,41 @@
+﻿using InventoryItems;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components
+{
+    [RequireComponent(typeof(Image))]
+    public class SelectedInventoryItemImageComponent : MonoBehaviour
+    {
+        private Image _selectedInventoryItemImage;
+
+        private void Awake()
+        {
+            _selectedInventoryItemImage = this.gameObject.GetComponent<Image>();
+
+            InventoryItemSelectionServiceOnSelectedInventoryItemChanged(
+                GameManager.Instance.InventoryItemSelectionService.SelectedInventoryItem);
+            GameManager.Instance.InventoryItemSelectionService.SelectedInventoryItemChanged +=
+                InventoryItemSelectionServiceOnSelectedInventoryItemChanged;
+        }
+        private void OnDestroy()
+        {
+            GameManager.Instance.InventoryItemSelectionService.SelectedInventoryItemChanged -=
+                InventoryItemSelectionServiceOnSelectedInventoryItemChanged;
+        }
+
+        private void InventoryItemSelectionServiceOnSelectedInventoryItemChanged(IInventoryItem inventoryItem)
+        {
+            if (inventoryItem == null)
+            {
+                _selectedInventoryItemImage.gameObject.SetActive(false);
+            }
+            else
+            {
+                _selectedInventoryItemImage.gameObject.SetActive(true);
+                _selectedInventoryItemImage.color = ((InventoryColor)inventoryItem).Color; // TODO
+            }
+        }
+    }
+}
