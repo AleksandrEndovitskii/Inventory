000880fd224e486b2aa304d0cfca1fe755f35249
Aleksandr <endovitsky.a.p@gmail.com>
2019-09-16 03:55:27 +0300

    added buttons input fields components

---
+++ Assets/Scripts/Components/InputFields/InputFieldComponent.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Components.InputFields
+{
+    [RequireComponent(typeof(TMP_InputField))]
+    public class InputFieldComponent : MonoBehaviour
+    {
+        private TMP_InputField _inputField;
+
+        private void Awake()
+        {
+            _inputField = this.gameObject.GetComponent<TMP_InputField>();
+
+            _inputField.onEndEdit.AddListener(OnEndEdit);
+        }
+        private void OnDestroy()
+        {
+            _inputField.onEndEdit.RemoveListener(OnEndEdit);
+        }
+
+        public virtual void OnEndEdit(string value)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Components/InputFields/SelectedColorInputField.cs
@@ -0,0 +1,30 @@
+﻿using InventoryItems;
+using Managers;
+using UnityEngine;
+
+namespace Components.InputFields
+{
+    public class SelectedColorInputField : InputFieldComponent
+    {
+        public override void OnEndEdit(string hexString)
+        {
+            var color = ((InventoryColor)GameManager.Instance.InventoryItemSelectionService.SelectedInventoryItem)
+                .Color;
+            ColorUtility.TryParseHtmlString(hexString, out color);
+
+            //TODO
+            // nothing changed
+            //if (color == ((InventoryColor)GameManager.Instance.InventoryItemSelectionService.SelectedInventoryItem)
+            //    .Color)
+            //{
+            //    _saveColorButton.interactable = false;
+
+            //    return;
+            //}
+
+            //_saveColorButton.interactable = true;
+
+            GameManager.Instance.ColorSelectionService.SelectedColor = color;
+        }
+    }
+}
